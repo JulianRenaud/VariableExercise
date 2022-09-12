1515831bccace4fbb7af7cf573a7960b28857efb
JulianRenaud <midway6220@gmail.com>
2022-09-12 11:38:00 -0500

    First Commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿string myFullName = "Julian Renaud";
+int myAge = 13;
+char myMiddleInitial = 'C';
+bool myAuthentication = true;
+double myCashBalance = 356.22;
+decimal myBankBalance = 87_549.11M;
+
+Console.WriteLine($"My full name is: {myFullName}\n" +
+                  $"My age is: {myAge}\n" +
+                  $"My middle initial is: {myMiddleInitial}\n" +
+                  $"My human authentication is: {myAuthentication}\n" +
+                  $"My cash balance is: {myCashBalance}\n" +
+                  $"My bank balance is: {myBankBalance}\n");
